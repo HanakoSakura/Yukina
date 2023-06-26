7e24896742a8766662fef996d9a1e2d66d30e236
HanakoSakura <vickysakura@dingtalk.com>
2023-06-26 19:10:40 +0800

    main

---
+++ Yukina.py
@@ -0,0 +1,15 @@
+
+# YUKINA MAIN
+
+import tools.sc as sc
+
+print('--YUKINA 0.1 ALPHA--')
+
+while True:
+    text = sc.scan_space(input('YUKINA '))
+    if len(text) > 0:
+        if text[0] == exit:
+            break
+        
+    
+    
