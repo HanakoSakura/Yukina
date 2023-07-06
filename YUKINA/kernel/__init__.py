c7ebff39df88db116dcb3a3b377e2da9fe9efb4e
HanakoSakura <vickysakura@dingtalk.com>
2023-07-06 13:49:49 +0800

    --

--- YUKINA/kernel/__init__.py
+++ YUKINA/kernel/__init__.py
@@ -2,10 +2,14 @@ from . import convert,CurseLib
 
 def maid(w:list[int],ctrl:dict)->list[int]:
     tmp = w
+    print('|####',end='')
     if ctrl.get('freq') != None:
         tmp = CurseLib.freq(tmp,ctrl['freq'])
+    print('####',end='')
     if ctrl.get('vol') != None:
         tmp = CurseLib.vol(tmp,ctrl['vol'])
+    print('####',end='')
     if ctrl.get('size') != None:
         tmp = CurseLib.repeat(tmp,ctrl['size'])
+    print('####',end='|\n')
     return tmp
